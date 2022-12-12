8138df08938d7c4b968a766a1001e98ee02f0936
shingelevich <63087390+poploveok@users.noreply.github.com>
2022-12-12 12:11:39 +0300

    split variables into declaration and initialization

--- src/test/java/tests/RegistrationWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationWithPageObjectsTests.java
@@ -3,6 +3,8 @@ package tests;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 /**
  * Page Object organisation
  *
@@ -22,9 +24,38 @@ import org.junit.jupiter.api.Test;
  */
 public class RegistrationWithPageObjectsTests extends TestBase {
 
-
     @Test
     void successfulRegistrationTest() {
+        /* String firstName = "Igor";
+    String lastName = "Shingelevich";
+    String userEmail = "shingelevich@gmail.com";
+    String userNumber = "1234567890";
+     String userDayDob = "22";
+    String userMonthDob = "January";
+    String userYearDob = "1985";
+    String currentAddress = "Moscow";
+    String userGenter = "Other";
+    List<String> userSubjects = List.of("Maths", "Arts", "Chemistry");
+    String userPicture = "src/test/java/resources/a_test_png_logo.png";
+    String userHobbies = "Sports";
+    String userState = "NCR";
+    String userCity = "Delhi";*/
+
+        firstName = "Igor";
+        lastName = "Shingelevich";
+        userEmail = "shingelevich@gmail.com";
+        userNumber = "1234567890";
+        userDayDob = "22";
+        userMonthDob = "January";
+        userYearDob = "1985";
+        currentAddress = "Moscow";
+        userGenter = "Other";
+        userSubjects = List.of("Maths", "Arts", "Chemistry");
+        userPicture = "src/test/java/resources/a_test_png_logo.png";
+        userHobbies = "Sports";
+        userState = "NCR";
+        userCity = "Delhi";
+
 
         registrationPage.openPage();
         registrationPage.setFirstName(firstName);
@@ -42,9 +73,8 @@ public class RegistrationWithPageObjectsTests extends TestBase {
         registrationPage.clickSubmitButton();
         registrationPage.openModalWindow();
         registrationPage.checkModalWindow(firstName, lastName, userEmail, userNumber, userGenter, userHobbies, currentAddress, userState, userCity);
-
-
-        /** TODO
+    }
+ /* TODO
              check the file upload,
              check the list of subjects
              represent Modal Window as Selenide Collection
@@ -52,11 +82,6 @@ public class RegistrationWithPageObjectsTests extends TestBase {
               do not understand uploadPicture method alternative                                                      https://github.com/MrDos180/demoqa_test/blob/ddfc0be12eede26a10962a9432ef7ec5c760f5a5/src/test/java/pages/RegistrationPage.java#L84
               do not understand how to implement stateLocator          $x("//div[text()='" + value + "']").click();     https://github.com/MrDos180/demoqa_test/blob/ddfc0be12eede26a10962a9432ef7ec5c760f5a5/src/test/java/pages/RegistrationPage.java#L96
               negative test case - check the error message
-
-
              */
-
-    }
-
 
 }
